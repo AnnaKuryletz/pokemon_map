586c7baa254e3f61b85d9b0f58cf4e2d5a04f5aa
anyukurilets <anyu.kutilez@mail.ru>
2025-01-28 15:00:10 +0300

    egister Pokemon model on admin UI

--- pokemon_entities/migrations/0001_initial.py
+++ pokemon_entities/migrations/0001_initial.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Pokemon',
+            name='Pokemons',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.TextField()),
